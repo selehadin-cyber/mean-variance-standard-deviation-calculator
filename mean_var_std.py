53f64acdc0e9ca491164cd55ab4c5a155d007381
selehaddin <sele4416@gmail.com>
2020-11-27 17:55:25 +0000

    👌upload ready solution

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,35 +1,44 @@
 import numpy as np
 
 def calculate(list):
-  a = np.array(list)
-  a.resize(3, 3)
-  print(a)
-  flattenedmean = np.mean(a)
-  flattenedvariance = np.var(a)
-  flattenedstandardD = np.std(a)
-  axis1mean = np.mean(a, axis=0)
-  axis2mean = np.mean(a, axis=1)
-  axis1variance = np.var(a, axis=0)
-  axis2variance = np.var(a, axis=1)
-  axis1std = np.var(a, axis=0)
-  axis2std = np.var(a, axis=1)
-  flattenedmax = np.max(a)
-  axis1max = np.max(a, axis=0)
-  axis2max = np.max(a, axis=1)
-  flattenedmin = np.min(a)
-  axis1min = np.min(a, axis=0)
-  axis2min = np.min(a, axis=1)
-  flattenedsum = np.sum(a)
-  axis1sum = np.sum(a, axis=0)
-  axis2sum = np.sum(a, axis=1)
-  calculations = {
-  'mean': [axis1mean.tolist(), axis2mean.tolist(), flattenedmean],
-  'variance': [axis1variance.tolist(), axis2variance.tolist(), flattenedvariance],
-  'standard deviation': [axis1std.tolist(), axis2std.tolist(), flattenedstandardD],
-  'max': [axis1max.tolist(), axis2max.tolist(), flattenedmax],
-  'min': [axis1min.tolist(), axis2min.tolist(), flattenedmin],
-  'sum': [axis1sum.tolist(), axis2sum.tolist(), flattenedsum]
-}
+    mean = []
+    variance = []
+    std = []
+    max = []
+    min = []
+    sum = []
+    if len(list) < 9:
+      raise ValueError ('List must contain nine numbers.')
+    else:
+        a = np.reshape(list,(3,3))
+    
+        mean.append(np.mean(a, axis=0).tolist())
+        mean.append(np.mean(a, axis=1).tolist()) 
+        mean.append(np.mean(a).tolist())
+        
+        variance.append(np.var(a, axis=0).tolist())
+        variance.append(np.var(a, axis=1).tolist())
+        variance.append(np.var(a).tolist())
 
+        std.append(np.std(a, axis=0).tolist()) 
+        std.append(np.std(a, axis=1).tolist())
+        std.append(np.std(a).tolist())
 
-  return calculations
+        max.append(np.max(a, axis=0).tolist()) 
+        max.append(np.max(a, axis=1).tolist())
+        max.append(np.max(a).tolist())
+
+        min.append(np.min(a, axis=0).tolist())
+        min.append(np.min(a, axis=1).tolist())
+        min.append(np.min(a).tolist())
+
+        sum.append(np.sum(a, axis=0).tolist())
+        sum.append(np.sum(a, axis=1).tolist())
+        sum.append(np.sum(a).tolist())
+        calculations = {'mean': mean,
+                        'variance': variance,
+                        'standard deviation': std,
+                        'max': max,
+                        'min': min,
+                        'sum': sum}
+    return calculations
